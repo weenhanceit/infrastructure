cd58f854e68d856680a940b188fd4840381a441c
Larry Reid <lcreid@jadesystems.ca>
2017-04-15 10:58:16 -0700

    Remove database from login.

--- basic-app-server/create-db-user.sh
+++ basic-app-server/create-db-user.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 psql -U root \
-  -d ${DATABASE:?"Please set DATABASE=database"} \
   -h weit-rds-a.cwfow80ezu1x.ca-central-1.rds.amazonaws.com <<EOF
 create role ${DATABASE_USERNAME:?"Please set DATABASE_USERNAME=username"}
   with createdb
