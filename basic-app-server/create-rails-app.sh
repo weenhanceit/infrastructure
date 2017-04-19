323f6ad9cfd2a49d0ef620315037f3b986f44b11
Larry Reid <lcreid@jadesystems.ca>
2017-04-19 13:28:06 -0700

    Multiple domains for Rails apps.

--- basic-app-server/create-rails-app.sh
+++ basic-app-server/create-rails-app.sh
@@ -1,13 +1,45 @@
 #!/bin/bash
 
+usage() {
+  echo usage: `basename $0` [-u user -hd] domain_name...
+  cat <<EOF
+  -d        Debug.
+  -u  user  Make created files and directories owned by user.
+  -h        This help.
+EOF
+}
+
+while getopts dhu: x ; do
+  case $x in
+    d) debug=1;;
+    u) user=$OPTARG;;
+    h) usage; exit 0;;
+    \?) echo Invalid option: -$OPTARG
+        usage
+        exit 1;;
+  esac
+done
+shift $((OPTIND-1))
+
 if [[ $# -lt 1 ]]; then
-  echo usage: $0 domain_name [site_owner]
+  usage
   exit 1
 fi
 
 domain_name=$1
+domain_names=""
+for d in "$@"; do
+  domain_names="${domain_names} $d www.$d"
+done
 root_directory=/var/www/$domain_name/html
-user=${2:-ubuntu}
+user=${user:-ubuntu}
+
+if [[ $debug ]]; then
+  echo Domain Names: $domain_names
+  echo Root Directory: $root_directory
+  echo User: $user
+  exit 0
+fi
 
 mkdir -p $root_directory
 chown -R $user:www-data $root_directory
@@ -30,7 +62,7 @@ upstream $domain_name {
 server {
   listen 80;
   listen [::]:80;
-  server_name $domain_name www.$domain_name;
+  server_name $domain_names;
 
   # http://stackoverflow.com/a/11313241/3109926 said the following
   # is what serves from public directly without hitting Puma
--- basic-app-server/create-server-block-rails.sh
+++ basic-app-server/create-server-block-rails.sh
@@ -2,14 +2,46 @@
 # Create a server block for nginx.
 # Started from: https://www.digitalocean.com/community/tutorials/how-to-set-up-nginx-server-blocks-virtual-hosts-on-ubuntu-14-04-lts
 
+usage() {
+  echo usage: `basename $0` [-u user -hd] domain_name...
+  cat <<EOF
+  -d        Debug.
+  -u  user  Make created files and directories owned by user.
+  -h        This help.
+EOF
+}
+
+while getopts dhu: x ; do
+  case $x in
+    d) debug=1;;
+    u) user=$OPTARG;;
+    h) usage; exit 0;;
+    \?) echo Invalid option: -$OPTARG
+        usage
+        exit 1;;
+  esac
+done
+shift $((OPTIND-1))
+
 if [[ $# -lt 1 ]]; then
-  echo usage: $0 domain_name [user]
+  usage
   exit 1
 fi
 
 domain_name=$1
+domain_names=""
+for d in "$@"; do
+  domain_names="${domain_names} $d www.$d"
+done
 root_directory=/var/www/$domain_name/html
-user=${2:-ubuntu}
+user=${user:-ubuntu}
+
+if [[ $debug ]]; then
+  echo Domain Names: $domain_names
+  echo Root Directory: $root_directory
+  echo User: $user
+  exit 0
+fi
 
 mkdir -p $root_directory
 
@@ -41,15 +73,14 @@ upstream app {
 server {
   listen 80;
   listen [::]:80;
-  server_name $domain_name www.$domain_name;
+  server_name $domain_names;
 
   # http://stackoverflow.com/a/11313241/3109926 said the following
   # is what serves from public directly without hitting Puma
   root $root_directory/public;
-  try_files \$uri/index.html \$uri @app;
+  try_files \$uri/index.html \$uri @$domain_name;
 
   location @app {
-    # TODO: I'm confused about what to put for proxy_pass.
     proxy_pass http://app;
     proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
     proxy_set_header Host \$http_host;
