dc565dbc1d8580525fbd249fc84ea9be1c180a69
Larry Reid <lcreid@jadesystems.ca>
2017-05-01 16:51:19 -0700

    Bjorn's optimizations, steps 1-3.

--- basic-app-server/create-rails-app.sh
+++ basic-app-server/create-rails-app.sh
@@ -80,11 +80,21 @@ EOF
 if [[ $use_port == 443 ]]; then
   cat >>$server_block_definition <<-EOF
   # TLS config from: http://nginx.org/en/docs/http/configuring_https_servers.html
+  # HTTP2 doesn't require encryption, but at last reading, no browsers support
+  # HTTP2 without TLS, so only do http2 when we have TLS.
+  listen 443 ssl http2;
+  listen [::]:443 ssl http2;
   # Let's Encrypt file names and locations from: https://certbot.eff.org/docs/using.html#where-are-my-certificates
-  listen 443 ssl;
-  listen [::]:443 ssl;
   ssl_certificate_key /etc/letsencrypt/live/$domain_name/privkey.pem;
   ssl_certificate     /etc/letsencrypt/live/$domain_name/fullchain.pem;
+
+  # Optimize TLS, from: https://www.bjornjohansen.no/optimizing-https-nginx, steps 1-3
+  ssl_session_cache shared:SSL:1m; # Enough for 4,000 sessions.
+  ssl_session_timeout 180m;
+  ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+  ssl_prefer_server_ciphers on;
+  ssl_ciphers ECDH+AESGCM:ECDH+AES256:ECDH+AES128:DH+3DES:!ADH:!AECDH:!MD5;
+  # Other steps TBD
 EOF
 else
   cat >>$server_block_definition <<-EOF
@@ -103,7 +113,7 @@ cat >>$server_block_definition <<-EOF
   location @$domain_name {
     # A Rails app should force "SSL" so that it generates redirects to HTTPS,
     # among other things.
-    # However, you want Nginx to handle the load of TLS.
+    # However, you want Nginx to handle the workload of TLS.
     # The trick to proxying to a Rails app, therefore, is to proxy pass to HTTP,
     # but set the header to HTTPS
     # Next two lines.
