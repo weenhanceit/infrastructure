35311467784cf8407b0309a41dbc852ff4971536
Larry Reid <lcreid@jadesystems.ca>
2017-10-30 07:50:41 -0700

    Change names.

---
+++ lib/shared_infrastructure.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "shared_infrastructure/nginx/nginx.rb"
+require "shared_infrastructure/nginx/server_block.rb"
+require "shared_infrastructure/nginx/server.rb"
+require "shared_infrastructure/nginx/lines.rb"
+require "shared_infrastructure/nginx/listen.rb"
+require "shared_infrastructure/nginx/location.rb"
+require "shared_infrastructure/nginx/upstream.rb"
+require "shared_infrastructure/nginx/site.rb"
+require "shared_infrastructure/nginx/builder.rb"
+require "shared_infrastructure/runner/base.rb"
+require "shared_infrastructure/runner/reverse_proxy.rb"
+require "shared_infrastructure/runner/static_site.rb"
+require "shared_infrastructure/systemd/systemd.rb"
+require "shared_infrastructure/systemd/rails.rb"
--- test/reverse_proxy_runner_test.rb
+++ test/reverse_proxy_runner_test.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require "minitest/autorun"
-require "bcon_infrastructure"
+require "shared_infrastructure"
 require "test"
 
 class ReverseProxyRunnerTest < Test
