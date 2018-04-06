9b4b40e5a582d27193ef69984a298786830975d3
Larry Reid <lcreid@jadesystems.ca>
2018-04-06 14:13:40 -0700

    Fix service name in systemctl enable.

--- lib/shared_infrastructure/systemd/rails.rb
+++ lib/shared_infrastructure/systemd/rails.rb
@@ -67,7 +67,7 @@ module Systemd
         puts "changing mode of unit file" if Runner.debug
         FileUtils.chmod(0o600, Systemd.unit_file(domain_name))
         puts "enabling service" if Runner.debug && Process.uid.zero?
-        `sudo systemctl enable $domain_name.service` if Process.uid.zero?
+        `systemctl enable #{domain_name}.service` if Process.uid.zero?
 
         result
       end
