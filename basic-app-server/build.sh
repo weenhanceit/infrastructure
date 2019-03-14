ffa79749021e8e15f462df8aec05b2bf8b3ac4a1
Larry Reid <lcreid@jadesystems.ca>
2019-03-14 14:37:17 -0700

    Update for 18.04.

--- basic-app-server/build.sh
+++ basic-app-server/build.sh
@@ -7,7 +7,7 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" |
 
 sudo apt-get update -y -qq
 
-sudo apt-get install -y -q linux-headers-$(uname -r) build-essential dkms
+sudo apt-get install -y -q linux-headers-generic build-essential dkms
 sudo apt-get install git
 sudo apt-get install -y -q ruby ruby-dev
 
@@ -18,57 +18,20 @@ sudo apt-get install -y -q patch zlib1g-dev liblzma-dev
 sudo apt-get install -y -q pdftk
 
 sudo apt-get install -y -q postgresql-client libpq-dev
-sudo apt-get install -y -q sqlite3 libsqlite3-dev
 sudo apt-get install -y -q nodejs nginx yarn
 
 # Install Redis
-# Adapted from: https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-redis-on-ubuntu-16-04
-# make test hangs on 4.0. Internet suggests: `taskset -c 1 make test`
-# https://github.com/antirez/redis/issues/1417
-# But above obviously doesn't work on single CPU Vagrant box.
-# 3.2 passed test at least once.
-# The version in the Ubuntu 16.04 repository is quite old (2.3)
-# At the time of writing, the most recent version of 3 was 3.2,
-# and 4.0 was already in use.
-cd /tmp
-sudo apt-get install -y -q tcl
-curl -O http://download.redis.io/releases/redis-3.2.11.tar.gz
-tar -xzvf redis-3.2.11.tar.gz
-cd redis-3.2.11
-# curl -O http://download.redis.io/redis-stable.tar.gz
-# tar -xzvf redis-stable.tar.gz
-# cd redis-stable
-make
-sudo make install
-sudo adduser --system --group --no-create-home redis
-sudo mkdir /var/lib/redis #/var/log/redis
-sudo chown redis:redis /var/lib/redis #/var/log/redis
-sudo chmod 770 /var/lib/redis #/var/log/redis
+sudo apt-get install -y -q redis redis-tools
+
 sudo sed -i.original \
   -e '/^supervised no/s/no/systemd/' \
   -e '/^dir/s;.*;dir /var/lib/redis;' \
-  redis.conf
-sudo cp redis.conf /etc
-cat >redis.service <<EOF
-[Unit]
-Description=Redis In-Memory Data Store
-After=network.target
+  /etc/redis/redis.conf
 
-[Service]
-User=redis
-Group=redis
-ExecStart=/usr/local/bin/redis-server /etc/redis.conf
-ExecStop=/usr/local/bin/redis-cli shutdown
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
-EOF
-sudo cp redis.service /etc/systemd/system/
 echo This build does NOT start Redis.
 echo To enable automatic start of Redis on system start, type:
 echo sudo systemctl enable redis
-cd ..
+
 # Sendmail
 sudo apt-get install -y -q sendmail
 
@@ -93,4 +56,4 @@ sudo apt-get upgrade -y -qq
 sudo apt-get dist-upgrade -y -qq
 sudo apt-get autoremove -y -qq
 
-sudo gem install rails -v 5.0.1 --no-document
+sudo gem install rails -v 5.2.2.1 --no-document
